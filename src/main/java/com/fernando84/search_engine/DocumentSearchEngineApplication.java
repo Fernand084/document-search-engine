95d6198b27fb30627ae3841bdb1c0b1cfb10974e
Fernand084 <edgar.lopezr84@outlook.com>
2025-09-25 15:36:36 -0600

    project setup, first commit

---
+++ src/main/java/com/fernando84/search_engine/DocumentSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.fernando84.search_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando84/search_engine/DocumentSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando84.search_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
